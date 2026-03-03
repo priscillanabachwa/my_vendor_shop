8efa4255a1fd0c8fd59e956321f602e7b10d4775
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-03-03 22:13:18 +0300

    started to create a model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Item (models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+
+    
+
 # Create your models here.
